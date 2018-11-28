4e3fbc06dda742ddd812796b244336d652a43bc9
plOnion <damian.lukasiewicz@gmail.com>
2018-11-28 22:39:02 +0100

    v 0.99 - slider fixed

--- src/slider.js
+++ src/slider.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Carousel from 'react-image-carousel';
+import slide1 from "./img/slide1.jpg";
+import slide2 from "./img/slide2.jpg";
+import slide3 from "./img/slide3.png";
+
 
 class SimpleSlider extends React.Component {
 
     render() {
         let images = [
-            './img/slide1.jpg',
-            './img/slide2.jpg',
-            './img/slide3.png',
+            slide1,
+            slide2,
+            slide3,
         ]
 
         return (
-            <div className="my-carousel">
+            <div className="my-carousel main_slider">
                 <Carousel images={images}
                           thumb={true}
                           loop={true}
